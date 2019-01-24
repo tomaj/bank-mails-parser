941816316e97c56df1ccdfd0484bd923d2952b92
Daniel Janiga <daniel.janiga@pixwell.sk>
2019-01-24 10:25:33 +0100

    fixed codestyle

--- tests/TatraBankaMailParserTest.php
+++ tests/TatraBankaMailParserTest.php
@@ -1,14 +1,14 @@
 <?php
 
-require dirname(__FILE__). '/../vendor/autoload.php';
+require dirname(__FILE__) . '/../vendor/autoload.php';
 
 use Tomaj\BankMailsParser\Parser\TatraBankaMailParser;
 
 class TatraBankaMailParserTest extends PHPUnit_Framework_TestCase
 {
-	public function testSimpleEmail()
-	{
-		$email = 'Vazeny klient,
+    public function testSimpleEmail()
+    {
+        $email = 'Vazeny klient,
 
 16.1.2015 12:51 bol zostatok Vasho uctu SK9812353347235 zvyseny o 12,31 EUR.
 uctovny zostatok:                            142,11 EUR
@@ -29,23 +29,23 @@ Poznamka: Vase pripomienky alebo otazky tykajuce sa tejto spravy alebo inej nase
 
 Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu uctu najdete v ucelenom tvare v pohyboch cez internet banking a nemusite ju pracne skladat zo starych bmailov.
 ';
-		$tatrabankaMailParser = new TatraBankaMailParser();
-		$mailContent = $tatrabankaMailParser->parse($email);
+        $tatrabankaMailParser = new TatraBankaMailParser();
+        $mailContent = $tatrabankaMailParser->parse($email);
 
-		$this->assertEquals('SK9812353347235', $mailContent->getAccountNumber());
-		$this->assertEquals('EUR', $mailContent->getCurrency());
-		$this->assertEquals(12.31, $mailContent->getAmount());
-		$this->assertEquals('1234056789', $mailContent->getVs());
-		$this->assertEquals('test-sprava', $mailContent->getReceiverMessage());
+        $this->assertEquals('SK9812353347235', $mailContent->getAccountNumber());
+        $this->assertEquals('EUR', $mailContent->getCurrency());
+        $this->assertEquals(12.31, $mailContent->getAmount());
+        $this->assertEquals('1234056789', $mailContent->getVs());
+        $this->assertEquals('test-sprava', $mailContent->getReceiverMessage());
         $this->assertEquals('CCINT 1100/000000-261426464', $mailContent->getDescription());
-		$this->assertNull($mailContent->getKs());
-		$this->assertNull($mailContent->getSs());
-		$this->assertEquals(strtotime('16.1.2015 12:51'), $mailContent->getTransactionDate());
-	}
+        $this->assertNull($mailContent->getKs());
+        $this->assertNull($mailContent->getSs());
+        $this->assertEquals(strtotime('16.1.2015 12:51'), $mailContent->getTransactionDate());
+    }
 
-	public function testAllInputsWithDecreaseEmail()
-	{
-		$email = 'Vazeny klient,
+    public function testAllInputsWithDecreaseEmail()
+    {
+        $email = 'Vazeny klient,
 
 16.1.2015 12:11 bol zostatok Vasho uctu SK9812353347235 znizeny o 43,29 USD.
 uctovny zostatok:                            22,11 EUR
@@ -66,23 +66,23 @@ Poznamka: Vase pripomienky alebo otazky tykajuce sa tejto spravy alebo inej nase
 
 Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu uctu najdete v ucelenom tvare v pohyboch cez internet banking a nemusite ju pracne skladat zo starych bmailov.
 ';
-		$tatrabankaMailParser = new TatraBankaMailParser();
-		$mailContent = $tatrabankaMailParser->parse($email);
+        $tatrabankaMailParser = new TatraBankaMailParser();
+        $mailContent = $tatrabankaMailParser->parse($email);
 
-		$this->assertEquals('SK9812353347235', $mailContent->getAccountNumber());
-		$this->assertEquals('USD', $mailContent->getCurrency());
-		$this->assertEquals(-43.29, $mailContent->getAmount());
-		$this->assertEquals('1234056789', $mailContent->getVs());
-		$this->assertEquals('9087654321', $mailContent->getSs());
-		$this->assertEquals('5428175648', $mailContent->getKs());
-		$this->assertEquals('test-sprava-druha', $mailContent->getReceiverMessage());
+        $this->assertEquals('SK9812353347235', $mailContent->getAccountNumber());
+        $this->assertEquals('USD', $mailContent->getCurrency());
+        $this->assertEquals(-43.29, $mailContent->getAmount());
+        $this->assertEquals('1234056789', $mailContent->getVs());
+        $this->assertEquals('9087654321', $mailContent->getSs());
+        $this->assertEquals('5428175648', $mailContent->getKs());
+        $this->assertEquals('test-sprava-druha', $mailContent->getReceiverMessage());
         $this->assertEquals('CCINT 1100/000000-261426464', $mailContent->getDescription());
-		$this->assertEquals(strtotime('16.1.2015 12:11'), $mailContent->getTransactionDate());
-	}
+        $this->assertEquals(strtotime('16.1.2015 12:11'), $mailContent->getTransactionDate());
+    }
 
-	public function testEmailWithouReceiverMessage()
-	{
-		$email = 'Vazeny klient,
+    public function testEmailWithouReceiverMessage()
+    {
+        $email = 'Vazeny klient,
 
 16.1.2015 12:11 bol zostatok Vasho uctu SK9812353347235 znizeny o 1 243,29 USD.
 uctovny zostatok:                            100,00 EUR
@@ -103,19 +103,19 @@ Poznamka: Vase pripomienky alebo otazky tykajuce sa tejto spravy alebo inej nase
 Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu uctu najdete v ucelenom tvare v pohyboch cez internet banking a nemusite ju pracne skladat zo starych bmailov.
 ';
 
-		$tatrabankaMailParser = new TatraBankaMailParser();
-		$mailContent = $tatrabankaMailParser->parse($email);
+        $tatrabankaMailParser = new TatraBankaMailParser();
+        $mailContent = $tatrabankaMailParser->parse($email);
 
-		$this->assertEquals('SK9812353347235', $mailContent->getAccountNumber());
-		$this->assertEquals('USD', $mailContent->getCurrency());
-		$this->assertEquals(-1243.29, $mailContent->getAmount());
-		$this->assertEquals('1234056789', $mailContent->getVs());
-		$this->assertEquals('9087654321', $mailContent->getSs());
-		$this->assertEquals('5428175648', $mailContent->getKs());
-		$this->assertNull($mailContent->getReceiverMessage());
+        $this->assertEquals('SK9812353347235', $mailContent->getAccountNumber());
+        $this->assertEquals('USD', $mailContent->getCurrency());
+        $this->assertEquals(-1243.29, $mailContent->getAmount());
+        $this->assertEquals('1234056789', $mailContent->getVs());
+        $this->assertEquals('9087654321', $mailContent->getSs());
+        $this->assertEquals('5428175648', $mailContent->getKs());
+        $this->assertNull($mailContent->getReceiverMessage());
         $this->assertEquals('CCINT 1100/000000-261426464', $mailContent->getDescription());
-		$this->assertEquals(strtotime('16.1.2015 12:11'), $mailContent->getTransactionDate());
-	}
+        $this->assertEquals(strtotime('16.1.2015 12:11'), $mailContent->getTransactionDate());
+    }
 
     public function testEmailWithouDescription()
     {
@@ -154,9 +154,9 @@ Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu u
         $this->assertEquals(strtotime('16.1.2015 12:11'), $mailContent->getTransactionDate());
     }
 
-	public function testEmailWithoutVariableSymbol()
-	{
-		$email = 'Vazeny klient,
+    public function testEmailWithoutVariableSymbol()
+    {
+        $email = 'Vazeny klient,
 
 12.1.2015 12:11 bol zostatok Vasho uctu SK9812369347235 znizeny o 2,20 EUR.
 uctovny zostatok:                            32,52 EUR
@@ -177,23 +177,23 @@ Poznamka: Vase pripomienky alebo otazky tykajuce sa tejto spravy alebo inej nase
 Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu uctu najdete v ucelenom tvare v pohyboch cez internet banking a nemusite ju pracne skladat zo starych bmailov.
 ';
 
-		$tatrabankaMailParser = new TatraBankaMailParser();
-		$mailContent = $tatrabankaMailParser->parse($email);
+        $tatrabankaMailParser = new TatraBankaMailParser();
+        $mailContent = $tatrabankaMailParser->parse($email);
 
-		$this->assertEquals('SK9812369347235', $mailContent->getAccountNumber());
-		$this->assertEquals('EUR', $mailContent->getCurrency());
-		$this->assertEquals(-2.20, $mailContent->getAmount());
-		$this->assertNull($mailContent->getVs());
-		$this->assertEquals('9087654322', $mailContent->getSs());
-		$this->assertEquals('5428175649', $mailContent->getKs());
-		$this->assertNull($mailContent->getReceiverMessage());
+        $this->assertEquals('SK9812369347235', $mailContent->getAccountNumber());
+        $this->assertEquals('EUR', $mailContent->getCurrency());
+        $this->assertEquals(-2.20, $mailContent->getAmount());
+        $this->assertNull($mailContent->getVs());
+        $this->assertEquals('9087654322', $mailContent->getSs());
+        $this->assertEquals('5428175649', $mailContent->getKs());
+        $this->assertNull($mailContent->getReceiverMessage());
         $this->assertEquals('CCINT 1100/000000-261426464', $mailContent->getDescription());
-		$this->assertEquals(strtotime('12.1.2015 12:11'), $mailContent->getTransactionDate());
-	}
+        $this->assertEquals(strtotime('12.1.2015 12:11'), $mailContent->getTransactionDate());
+    }
 
-	public function testEmailWithVariableSymbolInMessage()
-	{
-		$email = 'Vazeny klient,
+    public function testEmailWithVariableSymbolInMessage()
+    {
+        $email = 'Vazeny klient,
 
 12.1.2015 12:11 bol zostatok Vasho uctu SK9812369347235 znizeny o 2,20 EUR.
 uctovny zostatok:                            32,52 EUR
@@ -215,23 +215,23 @@ Poznamka: Vase pripomienky alebo otazky tykajuce sa tejto spravy alebo inej nase
 Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu uctu najdete v ucelenom tvare v pohyboch cez internet banking a nemusite ju pracne skladat zo starych bmailov.
 ';
 
-		$tatrabankaMailParser = new TatraBankaMailParser();
-		$mailContent = $tatrabankaMailParser->parse($email);
+        $tatrabankaMailParser = new TatraBankaMailParser();
+        $mailContent = $tatrabankaMailParser->parse($email);
 
-		$this->assertEquals('SK9812369347235', $mailContent->getAccountNumber());
-		$this->assertEquals('EUR', $mailContent->getCurrency());
-		$this->assertEquals(-2.20, $mailContent->getAmount());
-		$this->assertEquals('1234056789', $mailContent->getVs());
-		$this->assertNull($mailContent->getSs());
-		$this->assertNull($mailContent->getKs());
-		$this->assertEquals('Message', $mailContent->getReceiverMessage());
+        $this->assertEquals('SK9812369347235', $mailContent->getAccountNumber());
+        $this->assertEquals('EUR', $mailContent->getCurrency());
+        $this->assertEquals(-2.20, $mailContent->getAmount());
+        $this->assertEquals('1234056789', $mailContent->getVs());
+        $this->assertNull($mailContent->getSs());
+        $this->assertNull($mailContent->getKs());
+        $this->assertEquals('Message', $mailContent->getReceiverMessage());
         $this->assertEquals('CCINT 1100/000000-261426464', $mailContent->getDescription());
-		$this->assertEquals(strtotime('12.1.2015 12:11'), $mailContent->getTransactionDate());
-	}
+        $this->assertEquals(strtotime('12.1.2015 12:11'), $mailContent->getTransactionDate());
+    }
 
-	public function testErrorEmail()
-	{
-		$email = '4321/KS5428175648
+    public function testErrorEmail()
+    {
+        $email = '4321/KS5428175648
 Informacia pre prijemcu: test-sprava-druha
 
 S pozdravom
@@ -244,9 +244,9 @@ Poznamka: Vase pripomienky alebo otazky tykajuce sa tejto spravy alebo inej nase
 
 Odporucame Vam mazat si po precitani prichadzajuce bmail notifikacie. Historiu uctu najdete v ucelenom tvare v pohyboch cez internet banking a nemusite ju pracne skladat zo starych bmailov.
 ';
-		$tatrabankaMailParser = new TatraBankaMailParser();
-		$mailContent = $tatrabankaMailParser->parse($email);
+        $tatrabankaMailParser = new TatraBankaMailParser();
+        $mailContent = $tatrabankaMailParser->parse($email);
 
-		$this->assertFalse($mailContent);
-	}
+        $this->assertFalse($mailContent);
+    }
 }
